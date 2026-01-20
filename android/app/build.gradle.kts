95e471f17ea862b35ce7b9cb2038b352ea34b8ea
wandersonmaximo <engenheirocivil.wandersonmaximo@hotmail.com>
2026-01-20 16:34:57 -0300

    Update build.gradle.kts for new SDK and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,38 +1,12 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+compileSdkVersion 34
 
-plugins {
-    id("com.android.application")
-    id("dev.flutter.flutter-gradle-plugin")
+defaultConfig {
+    applicationId "com.exemplo.orcamentos"
+    minSdkVersion 21
+    targetSdkVersion 34
+    versionCode 1
+    versionName "1.0"
 }
-
-android {
-    namespace = "io.flutter.examples.hello_world"
-    compileSdk = flutter.compileSdkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    defaultConfig {
-        applicationId = "io.flutter.examples.hello_world"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        named("release") {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
+dependencies {
+    implementation "androidx.exifinterface:exifinterface:1.3.7"
 }
